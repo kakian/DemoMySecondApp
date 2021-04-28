d337abae59a6aed5714e9c06d450ca25832d1d0b
kakian <hew_ka_kian@rp.edu.sg>
2021-04-28 10:30:43 +0800

    Added feature 1a

--- app/src/main/java/sg/edu/rp/c346/id18123456/demomysecondapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18123456/demomysecondapp/MainActivity.java
@@ -3,13 +3,14 @@ package sg.edu.rp.c346.id18123456.demomysecondapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //some dumb comment
-        //bbbb
+
     }
 }
